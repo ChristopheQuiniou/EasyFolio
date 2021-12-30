7a23a01814d17322462c0a811f275566b10a7c28
elyz3r <elyzer.ak@gmail.com>
2021-12-30 02:16:29 +0100

    starting DAOAccount

--- DAO/Account/DAOAccount.class.php
+++ DAO/Account/DAOAccount.class.php
@@ -2,24 +2,93 @@
 
 class DAOAccount extends DAO implements IDAO
 {
-
     public function create(object $obj): Boolean
     {
-        // TODO: Implement create() method.
+        try{
+            # $this->connect();
+            $query = "INSERT INTO Account VALUES(:id,:name,:surname,:birthdate,:adress,:phoneNumber,:emailAdress,:password,:profilPicture)";
+            $data = array(
+                'id'=>$this->db->getId(),
+                'name'=>$this->db->getName(),
+                'surname'=>$this->db->getSurname(),
+                'birthdate'=>$this->db->getBithdate(),
+                'adress'=>$this->db->getAdress(),
+                'phoneNumber'=>$this->db->getPhoneNumber(),
+                'emailAdress'=>$this->db->getEmailAdress(),
+                'password'=>$this->db->getPassword(),
+                'profilPicture'=>$this->db->getprofilPicture()
+            );
+            $sth = $this->instance->prepare( $query );
+            $res = $sth->execute( $data );
+            $this->instance = null;
+            return $res;
+        }catch (PDOException $e){
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 
     public function read(int $id): object
     {
-        // TODO: Implement read() method.
+        try{
+            # $this->connect();
+            $query = "SELECT * FROM Account";
+            $data = array(
+                ':id'=>$this->db->getId()
+            );
+            $sth = $this->instance->prepare( $query );
+            $res=$sth->execute( $data );
+            $this->instance = null;
+            return $res;
+        }
+        catch (PDOException $e){
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 
     public function update(object $obj): Boolean
     {
-        // TODO: Implement update() method.
+        try{
+            #$this->connect();
+            $query = " UPDATE Adresse SET name=:name, surname=:surname, birthdate=:birthdate, adress=:adress,phoneNumber=:phoneNumber, emailAdress=:emailAdress, password=:password,profilPicture=:profilPicture WHERE id=:id ";
+            $data = array(
+                'id'=>$this->db->getId(),
+                'name'=>$this->db->getName(),
+                'surname'=>$this->db->getSurname(),
+                'birthdate'=>$this->db->getBithdate(),
+                'adress'=>$this->db->getAdress(),
+                'phoneNumber'=>$this->db->getPhoneNumber(),
+                'emailAdress'=>$this->db->getEmailAdress(),
+                'password'=>$this->db->getPassword(),
+                'profilPicture'=>$this->db->getprofilPicture()
+            );
+            $sth = $this->instance->prepare( $query );
+            $res=$sth->execute( $data );
+            $this->instance = null;
+            return $res;
+        }catch (PDOException $e){
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 
     public function delete(object $obj): Boolean
     {
-        // TODO: Implement delete() method.
+        try{
+            # $this->connect();
+            $query = "DELETE FROM Account WHERE id=:id ";
+            $data = array(
+                ':id'=>$this->db->getId()
+            );
+            $sth = $this->instance->prepare( $query );
+            $res=$sth->execute( $data );
+            $this->instance = null;
+            return $res;
+        }
+        catch (PDOException $e){
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 }
